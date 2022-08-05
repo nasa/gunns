8c4d071527fbcc8387b95fe9d58d6f1927f4dfca
Jason Harvey <jason.l.harvey@nasa.gov>
2022-08-05 06:40:55 -0500

    (#43) remove Trick ICG workaround

--- core/GunnsFluidDistributedIf.hh
+++ core/GunnsFluidDistributedIf.hh
@@ -34,14 +34,6 @@ PROGRAMMERS:
 #include "software/SimCompatibility/TsSimCompatibility.hh"
 #include <vector>
 
-// TODO issue #43: this is a workaround for a Trick ICG problem in RHEL8.  ICG determines the wrong
-// address offsets for some variables in the GunnsFluidDistributedIfData class.  This workaround
-// causes Trick to use the backup 'compat15' tool to correctly determine the variable offsets.
-// Remove this if & when ICG can be fixed.
-// Note this causes Trick to issue warnings about the use of TRICK_ICG in sim builds.
-#ifdef TRICK_ICG
-#endif
-
 ////////////////////////////////////////////////////////////////////////////////////////////////////
 /// @brief    Fluid Distributed Interface Data
 ///

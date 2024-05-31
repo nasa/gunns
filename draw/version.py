402cd9132fcb418753052d8fc077a77413580c2d
Jason Harvey <jason.l.harvey@nasa.gov>
2024-05-31 08:16:56 -0500

    (#90) Added the GunnsDraw shape, updated export script to handle change to shape library compression, updated example space vehicle sim

--- draw/version.py
+++ draw/version.py
@@ -2,4 +2,4 @@
 # @copyright Copyright 2024 United States Government as represented by the Administrator of the
 #            National Aeronautics and Space Administration.  All Rights Reserved.
 #
-GUNNSDRAW_VERSION = '19.4.3'
+GUNNSDRAW_VERSION = '19.4.4'
